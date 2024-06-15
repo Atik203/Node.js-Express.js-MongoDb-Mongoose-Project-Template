ff9291636d4fcbdb04b0071987ef955c20607883
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-06-15 20:13:10 +0600

    added QueryBuilder for sorting ,filtering,searching and pagination

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,9 +1,12 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  base_url: process.env.BASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  bcrypt_salt: process.env.BCRYPT_SALT,
 };
