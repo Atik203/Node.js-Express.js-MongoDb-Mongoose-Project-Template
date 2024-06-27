7c92f8ef8f5cde585683edd8964a02556f84235b
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-06-27 18:35:33 +0600

    added jwt and cookie-parser to config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,4 +9,8 @@ export default {
   base_url: process.env.BASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   bcrypt_salt: process.env.BCRYPT_SALT,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_access_expiration: process.env.JWT_ACCESS_EXPIRATION,
+  jwt_refresh_expiration: process.env.JWT_REFRESH_EXPIRATION,
 };
