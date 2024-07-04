b9eba259a73d8f9d08ddb324cbfcbd455b7eeedf
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-04 14:50:02 +0600

    fix globalError handler format and RouteNotFound format

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,22 +1,74 @@
-import { NextFunction, Request, Response } from 'express';
-import { z } from 'zod';
+import { ErrorRequestHandler } from 'express';
+import mongoose from 'mongoose';
+import { ZodError } from 'zod';
+import config from '../config';
+import AppError from '../Errors/AppError';
+import handleCastError from '../Errors/handleCastError';
+import handleDuplicateError from '../Errors/handleDuplicateError';
+import handleValidationError from '../Errors/handleValidationError';
+import handleZodError from '../Errors/handleZodError';
+import { TErrorSources } from '../interface/error';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-export const globalErrorHandler = (
-  error: unknown,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+export const globalErrorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next,
 ) => {
-  if (error instanceof z.ZodError) {
-    res.status(400).json({
-      success: false,
-      message: error.errors.map((err) => err.message).join(', '),
-    });
+  let statusCode = 500;
+  let message = 'Internal Server Error';
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Internal Server Error',
+    },
+  ];
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error instanceof mongoose.Error.ValidationError) {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error instanceof mongoose.Error.CastError) {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error.code === 11000) {
+    const simplifiedError = handleDuplicateError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error instanceof AppError) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message,
+      },
+    ];
   } else if (error instanceof Error) {
-    res.status(500).json({ success: false, message: error.message });
-  } else {
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message,
+      },
+    ];
   }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? error.stack : null,
+  });
 };
